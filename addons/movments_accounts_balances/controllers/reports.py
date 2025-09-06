23c639699014758bd31a616a8840416178afd159
Gourav <saini.gourav@techxsherpa.com>
2025-09-06 20:01:09 +0530

    Logger in reports

--- addons/movments_accounts_balances/controllers/reports.py
+++ addons/movments_accounts_balances/controllers/reports.py
@@ -95,6 +95,7 @@ class ReportsAPI(http.Controller):
                 company_id, start_date, end_date, partner_id, account_id)
             
             logger.info(f"get_account_balance called Domain: {domain}")
+            logger.info(f"get_account_balance called summarize_column_by: {summarize_column_by}")
             try:
                 move_line_ids = self.get_analytic_move_line_ids(analytic_class_id)
                 if move_line_ids is not None:
@@ -138,6 +139,7 @@ class ReportsAPI(http.Controller):
             # Build search domain
             domain = self.build_report_domain(company_id, start_date, end_date)
             logger.info(f"get_profit_loss called Domain: {domain}")
+            logger.info(f"get_profit_loss called summarize_column_by: {summarize_column_by}")
 
             profit_loss = prepare_profit_loss_response(
                 request, start_date, end_date, int(company_id), domain, None, summarize_column_by
